07ba04a5198e7eea4cd8f6274969ac1fe300eef2
Juzcallmekaushik <76829835+juzcallmekaushik@users.noreply.github.com>
2023-12-20 11:41:47 +0800

    Add files via upload

---
+++ data.py
@@ -0,0 +1,264 @@
+reactionroles_data = {"Study Ping Roles": {
+            "Languages": [['🇬🇧', 'English Language', 941218859250225152],
+                          ['📖', 'English/World Literature', 941504895700320266],
+                          ['2️⃣', 'English as a Second Language', 941218932570869770],
+                          ['🇸🇦', 'Arabic', 945568732393140224], ['🇮🇳', 'Hindi', 942791804464541726],
+                          ['🇫🇷', 'French', 942791457104855041], ['🇩🇪', 'German', 945565232032542802],
+                          ['🇨🇳', 'Mandarin', 945565876764155934], ['🇪🇸', 'Spanish', 945568301860413501],
+                          ['🇵🇰', 'Urdu', 945568902396661770], ['🇲🇾', 'Malay', 945567198603280424]],
+            "Humanities and Social Sciences": [['💸', 'Economics', 944761951064567819],
+                                               ['🗺️', 'Geography', 942795887946633268],
+                                               ['🌐', 'Global Perspectives', 945561426305630240],
+                                               ['⚔️', 'History', 942794982333513738],
+                                               ['🕌', 'Islamiyat/Islamic Studies and Pakistan Studies',
+                                                945569054335303710],
+                                               ['🤝', 'Sociology', 949571133856952320]],
+            "Creative and Professional": [['🏦', 'Accounting', 941504697980825621],
+                                          ['🎨', 'Art and Design', 945568090895294564],
+                                          ['💰', 'Business Studies', 941224721100460082],
+                                          ['💻', 'Computer Science', 941219105808187402],
+                                          ['🖊️', 'Design Technology', 945569646436843530],
+                                          ['📱', 'ICT', 945562595912474654],
+                                          ['🎵', 'Music', 945569438571307019],
+                                          ['✈️', 'Travel and Tourism', 949569608594096148]],
+            "Mathematics and Sciences": [['🔢', 'Mathematics', 941224259018166322],
+                                         ['✳️', 'Additional/Further Mathematics', 945562270832922644],
+                                         ['♾️', 'International Mathematics', 941224492125011978],
+                                         ['💡', 'Physics', 941505928988078141], ['🧪', 'Chemistry', 942792081808699472],
+                                         ['🍀', 'Biology', 943360807352287364],
+                                         ['🔍', 'Coordinated and Combined Sciences', 941506098928689172],
+                                         ['🌲', 'Environmental Management', 943360586731905024],
+                                         ["🏃", 'Physical Education', 945567738452135956]],
+        },
+        "Subject Roles" : {
+            "Mathematics and Sciences": [
+                ["🔢", "Mathematics", 688354722251276308],
+                ["✳️", "Additional/Further Mathematics", 688355303808303170],
+                ["♾️", "International Mathematics", 871702273640787988],
+                ["💡", "Physics", 685837416443281493],
+                ["🧪", "Chemistry", 685837450895032336],
+                ["🍀", "Biology", 685837475939221770],
+                ["🔍", "Coordinated and Combined Sciences", 667769546475700235],
+                ["🌲", "Environmental Management", 688357525984509984],
+                ["🏃‍♂️", "Physical Education", 685837363003523097],
+            ],
+            "Humanities and Social Sciences": [
+                ['💸', 'Economics', 685836740774330378],
+                ['🗺️', 'Geography', 677411157434171404],
+                ['🌐', 'Global Perspectives', 688357284920819717],
+                ['⚔️', 'History', 668927421512155142],
+                ['🕌', 'Islamiyat/Islamic Studies and Pakistan Studies', 688359807203278986],
+                ['🧑‍🤝‍🧑', 'Sociology', 685837255138607182]
+            ],
+            "Creative and Professional": [
+                ['🏦', 'Accounting', 688356986798211074],
+                ['🎨', 'Art and Design', 688362197096988688],
+                ['💰', 'Business Studies', 688356500506148884],
+                ['💻', 'Computer Science', 685836973004423169],
+                ['🖊️', 'Design Technology', 685837280396705803],
+                ['👨‍💻', 'ICT', 688360519735967766],
+                ['🎵', 'Music', 688362250951852085],
+                ['✈️', 'Travel and Tourism', 685837171491471401],
+            ],
+            "Languages": [
+                ['🇬🇧', 'English Language', 685836834005319721],
+                ['📖', 'English/World Literature', 868056692942835742],
+                ['2️⃣', 'English as a Second Language', 932660913054580766],
+                ['🇸🇦', 'Arabic', 688361654852780048],
+                ['🇮🇳', 'Hindi', 871587296330260570],
+                ['🇫🇷', 'French', 685836866166980661],
+                ['🇩🇪', 'German', 688361164161548361],
+                ['🇨🇳', 'Mandarin', 688361596149563431],
+                ['🇪🇸', 'Spanish', 685836889990627335],
+                ['🇵🇰', 'Urdu', 871587728737849344],
+                ['🇲🇾', 'Malay', 886884445657890826]
+            ],
+            "International AS and A Levels": [
+                ['❓', 'Other Subjects', 853588511252545566],
+                ['➕', 'Maths', 853587095317643264],
+                ['➖', 'Further Maths', 853588416774930432],
+                ['🧲', 'Physics', 853586114380300298],
+                ['⚗️', 'Chemistry', 853586292435451904],
+                ['🌱', 'Biology', 853586450141413416],
+                ['💵', 'Economics', 883190243623333888],
+                ['🧾', 'Accounting', 883190176644468758],
+                ['🔡', 'English', 881366711645921310],
+                ['🖥️', 'Computer Science', 868323020769493034],
+                ['⌨️', 'Information Technology', 883190289211228170],
+                ['🧠', 'Psychology', 868324666102661140]
+            ]
+        },
+            "Server Roles" : {
+                "Server Roles" :
+                    [
+                        ['✅', 'Announcements', 704577419994071070],
+                        ['👀', 'Spectator', 788344643367469057],
+                        ['🚫', 'Focused/Study Mute', 883193012442779718]
+                    ]
+            },
+             "Session Roles" : {
+                 "Session" :
+                     [
+                         ['🟠', 'February - March 2022', 782273117321166888],
+                         ['🟡', 'May - June 2022', 782423045578948618],
+                         ['🟢', 'October - November 2022', 782423808271056917],
+                         ['🔵', 'February - March 2023', 853135207862370324],
+                         ['🟣', 'May - June 2023', 853135509488009216],
+                         ['🟤', 'October - November 2023', 853135836597583902],
+                         ['⚫', 'February - March 2024', 931662314074169364],
+                         ['⚪', 'May - June 2024', 921725505051459664],
+                         ['🔴', 'October - November 2024', 921725841677901905],
+                         ['🔲', 'IGCSE Alumni', 698285991110246441]
+                     ],
+              "Board" :
+                  [
+                      ['🔶', 'CAIE', 853578618961002526],
+                      ['🔷', 'Edexcel', 853578446177566720],
+                      ['♦️', 'AQA', 872306086991003690],
+                      ['💠', 'Not IGCSE', 698291687981580308]
+                  ]
+            },
+    "Color Roles" : {
+        "Colors" : [
+             ['🟡', 'Yellow', 902191854974541825],
+             ['🟢', 'Green', 946251282962870324],
+             ['🔵', 'Blue', 946251561540153354],
+             ['🟣', 'Pink', 878507552143450142],
+             ['🔴', 'Red', 903466559308177449],
+             ['⚫', 'Black', 904252997486395412],
+             ['🟦', 'Light Blue', 949665171931009104],
+             ['🔷', 'Dark Blue', 946696792349737000],
+        ]
+    }
+}
+
+helper_roles = {
+    576463745073807372: 696688133844238367,
+    576463729802346516: 696415409720786994,
+    576463717844254731: 696415478331211806,
+    576461721900679177: 697031993820446720,
+    576463988506886166: 697032184451563530,
+    579288532942848000: 854000279933812737,
+    579292149678342177: 697032265624060005,
+    576464244455899136: 863699698234032168,
+    576463493646123025: 697031812102225961,
+    576463562084581378: 697031148685230191,
+    576463593562701845: 697090437520949288,
+    579386214218465281: 888382475645120575,
+    576463609325158441: 776986644757610517,
+    576463682054389791: 697031853369983006,
+    576461701332074517: 697030773814853633,
+    576463472544710679: 697030911023120455,
+    871702123505655849: 871702647223255050,
+    576463638983082005: 697031447021748234,
+    868056343871901696: 697031555457220673,
+    576463799582851093: 697031087985262612,
+    576463769526599681: 697030991205892156,
+    576463668808646697: 697407528685797417,
+    576464116336689163: 697031043089170463,
+    576463907485646858: 697031773435068466,
+    871589653315190845: 848529485351223325,
+    576463811327033380: 697031605100740698,
+    576463820575211560: 697031649233338408,
+    576463893858222110: 697031735086546954,
+    576463832168398852: 697031692115771513,
+    871590306338971678: 871588409032990770,
+    886883608504197130: 886884656845312020,
+    576464270041022467: 863691773628907560,
+    697072778553065542: 578170681670369290,
+    929349933432193085: 929422215940825088,
+    881366448423993344: 988167800164069386,
+    853585485338247189: 968534459890675732,
+    853585554797101066: 968512358257410048,
+    868324934143856660: 968597904405196881,
+    853585747587891201: 972583036602417212,
+    853585657921667092: 988168508925939782,
+    868325088234205195: 968595069760323644,
+    883184595019890738: 995173576107892767,
+    853587786099982396: 968513578875379724,
+    883184638267383848: 988171672966291527,
+    883184662292361276: 994981893596532837,
+    947859228649992213: 949941010430033950,
+    578977030582960132: 1082457204298350622,
+    1097739264139206717: 1052852529970561104,
+    1032065926595629079: 1032067091131535451  # Test channel/role
+}
+
+subreddits = {"Languages": {
+    "First Language English": "https://sites.google.com/view/igcseresources/languages/first-language-english",
+    "Literature in English": "https://sites.google.com/view/igcseresources/languages/english-literature",
+    "Hindi as a second language": "https://sites.google.com/view/igcseresources/languages/hindi",
+    "French": "https://sites.google.com/view/igcseresources/languages/french",
+    "Other Languages": "https://sites.google.com/view/igcseresources/languages/other-languages"
+},
+
+"Humanities and Social Sciences": {
+    "Economics": "https://sites.google.com/view/igcseresources/humanities-and-social-sciences/economics",
+    "History": "https://sites.google.com/view/igcseresources/humanities-and-social-sciences/history",
+    "Geography": "https://sites.google.com/view/igcseresources/humanities-and-social-sciences/geography",
+    "Islamiyat": "https://sites.google.com/view/igcseresources/humanities-and-social-sciences/islamiyat",
+    "Global Perspectives": "https://sites.google.com/view/igcseresources/humanities-and-social-sciences/global-perspectives",
+    "Sociology": "https://sites.google.com/view/igcseresources/humanities-and-social-sciences/sociology",
+    "Pakistan Studies": "https://sites.google.com/view/igcseresources/humanities-and-social-sciences/pakistan-studies"
+},
+
+"Sciences": {
+    "Biology": "https://sites.google.com/view/igcseresources/sciences/biology",
+    "Chemistry": "https://sites.google.com/view/igcseresources/sciences/chemistry",
+    "Physics": "https://sites.google.com/view/igcseresources/sciences/physics",
+    "Combined/Coordinated Sciences": "https://sites.google.com/view/igcseresources/sciences/combinedcoordinated-sciences",
+    "Environmental Management": "https://sites.google.com/view/igcseresources/sciences/environmental-management",
+    "Physical Education": "https://sites.google.com/view/igcseresources/sciences/physical-education",
+    "Psychology": "https://sites.google.com/view/igcseresources/sciences/psychology"
+},
+"Mathematics": {
+    "Mathematics": "https://sites.google.com/view/igcseresources/mathematics/mathematics",
+    "Additional Mathematics": "https://sites.google.com/view/igcseresources/mathematics/additional-mathematics",
+    "International Mathematics": "https://sites.google.com/view/igcseresources/mathematics/international-mathematics"
+},
+
+"Creative and Professional": {
+    "ICT": "https://sites.google.com/view/igcseresources/professional-and-creative/ict",
+    "Computer Science": "https://sites.google.com/view/igcseresources/professional-and-creative/computer-science",
+    "Accounting": "https://sites.google.com/view/igcseresources/professional-and-creative/accounting",
+    "Business Studies": "https://sites.google.com/view/igcseresources/professional-and-creative/business-studies",
+    "Art and Design": "https://sites.google.com/view/igcseresources/professional-and-creative/art-and-design",
+    "Travel and Tourism": "https://sites.google.com/view/igcseresources/professional-and-creative/travel-and-tourism",
+    "Food and Nutrition": "https://sites.google.com/view/igcseresources/professional-and-creative/food-and-nutrition"
+}
+}
+
+study_roles = {
+    576463745073807372: 941505928988078141,
+    576463729802346516: 943360807352287364,
+    576463717844254731: 942792081808699472,
+    576461721900679177: 941506098928689172,
+    579288532942848000: 943360586731905024,
+    579292149678342177: 945567738452135956,
+    576464244455899136: 941504697980825621,
+    576463493646123025: 945568090895294564,
+    576463562084581378: 941224721100460082,
+    576463593562701845: 941219105808187402,
+    579386214218465281: 945569646436843530,
+    576463609325158441: 945562595912474654,
+    576463682054389791: 945569438571307019,
+    576461701332074517: 941224259018166322,
+    576463472544710679: 945562270832922644,
+    871702123505655849: 941224492125011978,
+    576463638983082005: 941218859250225152,
+    868056343871901696: 941504895700320266,
+    929349933432193085: 941218932570869770,
+    576463799582851093: 944761951064567819,
+    576463769526599681: 942795887946633268,
+    576463668808646697: 945561426305630240,
+    576464116336689163: 942794982333513738,
+    576463907485646858: 945568732393140224,
+    871589653315190845: 942791804464541726,
+    576463811327033380: 942791457104855041,
+    576463820575211560: 945565232032542802,
+    576463893858222110: 945565876764155934,
+    576463832168398852: 945568301860413501,
+    871590306338971678: 945568902396661770,
+    886883608504197130: 945567198603280424,
+    576464270041022467: 945569054335303710,
+}
