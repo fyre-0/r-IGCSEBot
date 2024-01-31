9a9cb8392427aa47ecd25b6adcad41d341f819ec
cascade! <52331239+cvscade@users.noreply.github.com>
2024-02-01 04:42:26 +0530

    feat: add pictures (#127)
    cc: @chirag350
    
    Co-authored-by: Chirag L. <me@chirag.dev>

--- src/bans.py
+++
@@ -1,8 +0,0 @@
-from bot import discord, bot
-
-async def is_banned(user, guild):
-    try:
-        await guild.fetch_ban(user)
-        return True
-    except:
-        return False
--- src/schemas/redis/User.py
+++
@@ -1,8 +0,0 @@
-from redis_om import Field
-from .ExtendedModel import ExtendedModel
-
-class User(ExtendedModel):
-    user_id: str = Field(primary_key=True)
-    playing: bool = False
-    subject: str = None
-    session_id: str = None
