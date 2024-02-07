fd666e5422c03fc2bef05c1a4ee8751243719d5f
Dawood Sulaiman Syed <151532509+11xdeveloper@users.noreply.github.com>
2024-02-07 20:32:09 +0500

    Fixed temp mods not being able to run moderation commands (#145)
    * refactor: cleaned up roles.py
    
    * fix: incorrect imports

--- src/roles.py
+++ src/roles.py
@@ -1,30 +1,20 @@
 from bot import discord, bot
-from constants import GUILD_ID, FORCED_MUTE_ROLE, MODERATOR_ROLES, CHAT_MODERATOR_ROLES, BOT_DEVELOPER_ROLES
+from constants import GUILD_ID, MODERATOR_ROLES, CHAT_MODERATOR_ROLE, BOT_DEVELOPER_ROLE
 
 async def has_role(member: discord.Member, role_name: str):
-    for role in member.roles:
-        if role.name.lower() == role_name.lower():
-            return True
-    return False
-    
+    return any(role_name.lower() == role.lower() for role in member.roles)
+
 async def get_role(role_name: str):
-    guild = bot.get_guild(GUILD_ID)
-    return discord.utils.get(guild.roles, name = role_name)
+    return discord.utils.get(bot.get_guild(GUILD_ID).roles, name=role_name)
 
 async def is_moderator(member: discord.Member):
-    roles = [role.id for role in member.roles]
-    if MODERATOR_ROLES in roles:
-        return True
-    elif member.guild_permissions.administrator:
-        return True
-    return False
-
+    return any(role in [role.id for role in member.roles] for role in MODERATOR_ROLES) or member.guild_permissions.administrator
 
 async def is_chat_moderator(member: discord.Member):
-    return CHAT_MODERATOR_ROLES in [role.id for role in member.roles]
+    return CHAT_MODERATOR_ROLE in [role.id for role in member.roles]
 
 async def is_bot_developer(member: discord.Member):
-    return BOT_DEVELOPER_ROLES in [role.id for role in member.roles]
+    return BOT_DEVELOPER_ROLE in [role.id for role in member.roles]
 
 async def is_server_booster(member: discord.Member):
     return await has_role(member, "Server Booster")
