ec38915a13659885806161d00d6bbd9f0ec0ffb4
Juzcallmekaushik <76829835+juzcallmekaushik@users.noreply.github.com>
2024-03-10 13:42:46 +0800

    Removed on_ready Bot Logging Issue

--- src/events/on_ready.py
+++ src/events/on_ready.py
@@ -58,30 +58,29 @@ async def on_ready():
         client = pymongo.MongoClient(LINK)
         db = client.IGCSEBot
         prefs = db["guild_preferences"]
-        results = prefs.find({"guild_id": guild.id})
-        for result in results:
-            botlogid = result["botlogs_channel"]
-            botlogs = bot.get_channel(botlogid)
-            if botlogs:
-                user = bot.user
-                format = "%d-%m-%Y"
-                embed = discord.Embed(
-                    title=f"{bot.user.display_name} restarted successfully!", colour=0x51BB56
-                )
-                embed.add_field(
-                    name="Bot Information",
-                    value=f"```Name: {bot.user}\nCreated on: {bot.user.created_at.strftime(format)}\nJoined on: {guild.get_member(bot.user.id).joined_at.strftime(format)}\nBeta: {BETA}\nVerified: {user.verified}\nNo. of guilds: {len(bot.guilds)}\nID: {user.id}```",
-                    inline=False,
-                )
-                embed.add_field(
-                    name="Guild Information",
-                    value=f"```Name: {guild.name}\nOwner: {guild.owner}\nCreated on: {guild.created_at.strftime(format)}\nMembers: {guild.member_count}\nBoosts: {guild.premium_subscription_count}\nID: {guild.id}```",
-                    inline=False,
-                )
-                embed.add_field(
-                    name="Channels & Commands",
-                    value=f"```No. of roles: {len(guild.roles)}\nNo. of users: {len(guild.humans)}\nNo. of bots: {len(guild.bots)}\nNo. of catagories: {len(guild.categories)}\nNo. of text-channels: {len(guild.text_channels)}\nNo. of voice-channels: {len(guild.voice_channels)}\nNo. of forum-channels: {len(guild.forum_channels)}\nNo. of slash-commands: {len(bot.get_all_application_commands())}```",
-                    inline=False,
-                )
-                embed.set_footer(text=f"{bot.user}", icon_url=bot.user.display_avatar.url)
-                await botlogs.send(embed=embed)
+        result = prefs.find_one({"guild_id": GUILD_ID})
+        botlogid = result["botlogs_channel"]
+        botlogs = bot.get_channel(botlogid)
+        if botlogs:
+            user = bot.user
+            format = "%d-%m-%Y"
+            embed = discord.Embed(
+                title=f"{bot.user.display_name} restarted successfully!", colour=0x51BB56
+            )
+            embed.add_field(
+                name="Bot Information",
+                value=f"```Name: {bot.user}\nCreated on: {bot.user.created_at.strftime(format)}\nJoined on: {guild.get_member(bot.user.id).joined_at.strftime(format)}\nBeta: {BETA}\nVerified: {user.verified}\nNo. of guilds: {len(bot.guilds)}\nID: {user.id}```",
+                inline=False,
+            )
+            embed.add_field(
+                name="Guild Information",
+                value=f"```Name: {guild.name}\nOwner: {guild.owner}\nCreated on: {guild.created_at.strftime(format)}\nMembers: {guild.member_count}\nBoosts: {guild.premium_subscription_count}\nID: {guild.id}```",
+                inline=False,
+            )
+            embed.add_field(
+                name="Channels & Commands",
+                value=f"```No. of roles: {len(guild.roles)}\nNo. of users: {len(guild.humans)}\nNo. of bots: {len(guild.bots)}\nNo. of catagories: {len(guild.categories)}\nNo. of text-channels: {len(guild.text_channels)}\nNo. of voice-channels: {len(guild.voice_channels)}\nNo. of forum-channels: {len(guild.forum_channels)}\nNo. of slash-commands: {len(bot.get_all_application_commands())}```",
+                inline=False,
+            )
+            embed.set_footer(text=f"{bot.user}", icon_url=bot.user.display_avatar.url)
+            await botlogs.send(embed=embed)
